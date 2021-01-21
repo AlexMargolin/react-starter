baa0155e0e1645889d89f849d73747b1693cb6a5
Alex Margolin <csight@msn.com>
2021-01-21 03:15:32 +0200

    [polish] webpack scripts separated into files

---
+++ scripts/utils.js
@@ -0,0 +1,20 @@
+const path = require("path")
+
+/**
+ * Append root path to passed args.
+ * @param args
+ */
+const appendRoot = (...args) => {
+  return path.resolve(path.dirname(__dirname), ...args)
+}
+
+/**
+ * Returns a value based on active the environment
+ * @param dev
+ * @param prod
+ */
+const envCmp = (dev, prod) => {
+  return process.env.NODE_ENV === "development" ? dev : prod
+}
+
+module.exports = { appendRoot, envCmp }
